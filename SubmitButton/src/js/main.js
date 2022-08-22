4bdcf12f5645c1a743785fba8f815eebe4c6a133
TheRedCraft <bananeananas29@gmail.com>
2022-08-22 22:15:34 +0200

    submit button

---
+++ SubmitButton/src/js/main.js
@@ -0,0 +1,14 @@
+const SUBMIT = document.querySelector('#submit');
+
+const submitForm = ($event) => {
+	if (!$event.target.classList.contains('active')) {
+		$event.target.classList.add('active');
+		setTimeout(() => {
+			$event.target.classList.remove('active');
+			$event.target.classList.add('done');
+			$event.target.innerHTML = 'Done';
+		}, 1000);
+	}
+};
+
+SUBMIT.addEventListener('click', submitForm);
