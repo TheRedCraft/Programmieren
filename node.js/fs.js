dd3aec875afef5d8fa280e02fd077a0495f0dd98
TheRedCraft <bananeananas29@gmail.com>
2022-09-08 17:31:17 +0200

    yes

--- node.js/fs.js
+++ node.js/fs.js
@@ -10,24 +10,24 @@ fs.readdir(currentpath, dirread);
 
 
 function dirread(err, files){
-    console.log('Verzeichnis ausgelesen ' + currentpath);
-    console.log(files);
 
     files.forEach(processfile);
 }
 
 function processfile(file) {
     let extension = path.extname(file);
-    console.log('Extension ist: '+ extension);
+
 
     fs.mkdir(currentpath + "/" + extension, function(){
-        console.log('Ordner wurde fertig erstellt!');
+
         fs.rename(currentpath + "/" + file, currentpath + "/" + extension + '/' + file, copySucces);
 
     });
 }
 
 
-function copySucces(){
-    console.log('Datei wurde verschoben');
+function copySucces(err){
+    if(err) {
+        console.log(err)
+    }
 }
